c18968c37fda87c05bc05be1fa7dafe56fe00ce6
Pedro Lajas <pedrolajas.gamedesigner@gmail.com>
2026-03-05 14:27:22 +0000

    Change String Working Correctly

---
+++ ChangeString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string text = Console.ReadLine();
+
+            Console.Write("Character: ");
+            char c = Convert.ToChar(Console.ReadLine());
+
+            foreach (char letter in text)
+            {
+                if (letter == c)
+                Console.Write('x');
+                else
+                Console.Write(letter);
+            }
+        }
+    }
+}
